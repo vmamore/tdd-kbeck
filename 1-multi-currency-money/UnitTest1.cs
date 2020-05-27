dde1f486b0a6d9bcd2d5ee130123c683cb9531b6
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-27 06:44:45 -0400

    criado classe para teste executar

--- 1-multi-currency-money/UnitTest1.cs
+++ 1-multi-currency-money/UnitTest1.cs
@@ -3,13 +3,24 @@ using Xunit;
 
 namespace multi_currency_money
 {
-    public class UnitTest1
+    public class DollarTests
     {
         [Fact]
         public void TestMultiplication(){
             Dollar five = new Dollar(5);
             five.times(2);
             Assert.Equal(10, five.amount);
+        }
+    }
+
+    public class Dollar {
+        public int amount;
+        public Dollar(int amount){
+
+        }
+
+        public void times(int multiplier){
+
         }
     }
 }
