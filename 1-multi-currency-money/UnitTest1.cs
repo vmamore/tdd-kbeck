bd7d39c5b8638b6e6dedfbaca1a05eab3086b338
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-27 06:42:29 -0400

    added failing test

--- 1-multi-currency-money/UnitTest1.cs
+++ 1-multi-currency-money/UnitTest1.cs
@@ -6,9 +6,10 @@ namespace multi_currency_money
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
-
+        public void TestMultiplication(){
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.Equal(10, five.amount);
         }
     }
 }
