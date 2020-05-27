99052a146c2e7ec2cf39014709e6e8ede228c6c5
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-27 06:49:50 -0400

    adicionado implementacao que passa o teste

--- 1-multi-currency-money/UnitTest1.cs
+++ 1-multi-currency-money/UnitTest1.cs
@@ -9,18 +9,18 @@ namespace multi_currency_money
         public void TestMultiplication(){
             Dollar five = new Dollar(5);
             five.times(2);
-            Assert.Equal(10, five.amount);
+            Assert.Equal(10, five.Amount);
         }
     }
 
     public class Dollar {
-        public int amount;
+        public int Amount { get; private set; }
         public Dollar(int amount){
-
+            this.Amount = amount;
         }
 
         public void times(int multiplier){
-
+            this.Amount *= multiplier;
         }
     }
 }
