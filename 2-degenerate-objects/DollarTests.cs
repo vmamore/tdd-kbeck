79f0c651f2e1df633bc08901eaf056970eb948e5
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-27 06:58:46 -0400

    copiado implementacao de dolar e test

---
+++ 2-degenerate-objects/DollarTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace degenerate_objects
+{
+    public class DollarTests
+    {
+        [Fact]
+        public void TestMultiplication(){
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.Equal(10, five.Amount);
+        }
+    }
+
+    public class Dollar {
+        public int Amount { get; private set; }
+        public Dollar(int amount){
+            this.Amount = amount;
+        }
+
+        public void times(int multiplier){
+            this.Amount *= multiplier;
+        }
+    }
+}
