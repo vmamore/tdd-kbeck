a50ff6812370c35d03460e5f431fa9adec333c47
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-28 19:27:42 -0400

    criado projeto para capitulo 5

---
+++ 5-francly-speaking/DollarTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+
+namespace fraancly_speaking
+{
+    public class DollarTests
+    {
+        [Fact]
+        public void TestMoreThanOneMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            Assert.Equal(new Dollar(10), five.times(2));;
+            Assert.Equal(new Dollar(15), five.times(3));
+        }
+
+        [Fact]
+        public void TestEquality()
+        {
+            Assert.True(new Dollar(5).Equals(new Dollar(5)));
+            Assert.False(new Dollar(5).Equals(new Dollar(6)));
+        }
+    }
+
+    public class Dollar
+    {
+        private int Amount;
+        public Dollar(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        public Dollar times(int multiplier)
+        {
+            return new Dollar(this.Amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Dollar dollar = (Dollar)obj;
+            return this.Amount == dollar.Amount;
+        }
+    }
+}
