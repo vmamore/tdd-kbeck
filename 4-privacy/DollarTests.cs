1d06c052f6e99334e21c6ada49b59ced9409cd9b
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-28 19:20:13 -0400

    criado projeto para capitulo 4 privacy

---
+++ 4-privacy/DollarTests.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+
+namespace privacy
+{
+    public class DollarTests
+    {
+        [Fact]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            Dollar result = five.times(2);
+            Assert.Equal(10, result.Amount);
+        }
+
+        [Fact]
+        public void TestMoreThanOneMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            Dollar product = five.times(2);
+            Assert.Equal(10, product.Amount);
+            product = five.times(3);
+            Assert.Equal(15, product.Amount);
+        }
+
+        [Fact]
+        public void TestEquality()
+        {
+            Assert.True(new Dollar(5).Equals(new Dollar(5)));
+            Assert.False(new Dollar(5).Equals(new Dollar(6)));
+        }
+    }
+
+    public class Dollar
+    {
+        public int Amount { get; private set; }
+        public Dollar(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        public Dollar times(int multiplier)
+        {
+            return new Dollar(this.Amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Dollar dollar = (Dollar)obj;
+            return this.Amount == dollar.Amount;
+        }
+    }
+}
