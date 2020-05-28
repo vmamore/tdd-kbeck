4d2a854fdbe951699c6f576c3bf05b44a87e7a0a
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-28 19:32:35 -0400

    copied dollar to franc implementation

--- 5-francly-speaking/FrancTests.cs
+++ 5-francly-speaking/FrancTests.cs
@@ -11,4 +11,24 @@ namespace francly_speaking
             Assert.Equal(new Franc(15), five.Times(3));
         }
     }
+
+    public class Franc
+    {
+        private int Amount;
+        public Franc(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(this.Amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Franc dollar = (Franc)obj;
+            return this.Amount == dollar.Amount;
+        }
+    }
 }
