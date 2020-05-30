3a71f4ec97936a62253768892655149ca2c6ee01
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-30 17:39:04 -0400

    copied files

---
+++ 7-apples-and-oranges/FrancTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+namespace equality_for_all_redux
+{
+    public class FrancTests
+    {
+        [Fact]
+        public void TestFrancMultiplication(){
+            Franc five = new Franc(5);
+            Assert.Equal(new Franc(10), five.Times(2));
+            Assert.Equal(new Franc(15), five.Times(3));
+        }
+    }
+
+    public class Franc : Money
+    {
+        public Franc(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(this.Amount * multiplier);
+        }
+    }
+}
---
+++ 7-apples-and-oranges/Money.cs
@@ -0,0 +1,13 @@
+namespace apples_and_oranges
+{
+    public class Money
+    {
+        public int Amount { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            Money money = (Money)obj;
+            return this.Amount == money.Amount;
+        }
+    }
+}
