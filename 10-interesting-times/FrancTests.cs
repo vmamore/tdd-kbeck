a63806bfd6f9057b99b805d81a76216c3f3e5e5b
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-31 18:25:48 -0400

    criado projeto para capitulo 10: interesting times

---
+++ 10-interesting-times/FrancTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+
+namespace interesting_times
+{
+    public class FrancTests
+    {
+        [Fact]
+        public void TestFrancMultiplication(){
+            Franc five = new Franc(5, null);
+            Assert.Equal(new Franc(10, null), five.Times(2));
+            Assert.Equal(new Franc(15, null), five.Times(3));
+        }
+    }
+
+    public class Franc : Money
+    {
+        public Franc(int amount, string currency) : base(amount, currency)
+        {
+        }
+        public override string Currency => _currency;
+        public override Money Times(int multiplier)
+        {
+            return new Franc(this.Amount * multiplier, null);
+        }
+    }
+}
