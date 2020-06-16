e46321df57e38145f7807a7f0bb16077bcba1e9d
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-06-15 23:56:38 -0400

    conclusao de cap 19

--- 19-set-the-table/index.py
+++ 19-set-the-table/index.py
@@ -3,8 +3,12 @@ class TestCase:
         self.name = name
     
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+    
+    def setUp(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self,name):
@@ -13,13 +17,22 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+    
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
     
 TestCaseTest("testRunning").run()
 
