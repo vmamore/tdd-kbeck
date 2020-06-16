c4984926ec00cf297ab106b19caef2a292ec3329
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-06-15 23:58:04 -0400

    criado pasta para capitulo 20

---
+++ 20-cleaning-up-after/index.py
@@ -0,0 +1,39 @@
+class TestCase:
+    def __init__(self,name):
+        self.name = name
+    
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+    
+    def setUp(self):
+        pass
+
+class WasRun(TestCase):
+    def __init__(self,name):
+        self.wasRun = None
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = 1
+    
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+    
+TestCaseTest("testRunning").run()
+
+
